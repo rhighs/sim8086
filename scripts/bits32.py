8f950f135f5e54fb8347e6e8bc92a05b2543379c
Roberto Montalti <roberto.montalti00@gmail.com>
2023-04-09 17:06:24 +0200

    Utility script showing bits of a file

---
+++ scripts/bits32.py
@@ -0,0 +1,12 @@
+import sys
+with open(sys.argv[1], 'rb') as file:
+    bytes = file.read()
+    i = 0
+    for b in bytes:
+        if i == 4:
+            i = 0
+            print("")
+        i+=1
+        for j in reversed(range(0, 8)):
+            print("1" if (b >> j) & 0x1 == 1 else "0", end="")
+    print("")
