486931f9cbad6c1b3c6dadaa8916c3378bc54a81
Roberto Montalti <roberto.montalti00@gmail.com>
2023-04-09 16:52:20 +0200

    Simple decoding of many movs

--- cpu.c
+++ cpu.c
@@ -30,9 +30,22 @@ typedef int64_t i64;
 #define REG_SI 0x6
 #define REG_DI 0x7
 
+#define IMOV 0b00100010
+
+void __print_bits(const u32 n) {
+    u8 len = sizeof(n) * 8;
+    while (len--) {
+        if ((len+1) % 8 == 0 && (len+1) < sizeof(n) * 8) {
+            printf(" ");
+        }
+        printf("%c", ((n >> len) & 0x1) ? '1' : '0');
+    }
+    printf("\n");
+}
+
 void regcode_to_str(const u8 code, const u8 w_bit, char *out) {
     char *__out;
-    if (w_bit) switch (code) {
+    if (!w_bit) switch (code) {
         case REG_AL: __out = "al"; break;
         case REG_CL: __out = "cl"; break;
         case REG_DL: __out = "dl"; break;
@@ -41,8 +54,7 @@ void regcode_to_str(const u8 code, const u8 w_bit, char *out) {
         case REG_CH: __out = "ch"; break;
         case REG_DH: __out = "dh"; break;
         case REG_BH: __out = "bh"; break;
-    }
-    if (!w_bit) switch (code) {
+    } else switch (code) {
         case REG_AX: __out = "ax"; break;
         case REG_CX: __out = "cx"; break;
         case REG_DX: __out = "dx"; break;
@@ -56,24 +68,98 @@ void regcode_to_str(const u8 code, const u8 w_bit, char *out) {
 }
 
 void instrcode_to_str(const u8 code, char *out) {
+    char *__out;
+    switch (code) {
+        case IMOV:
+            __out = "mov";
+            break;
+    }
+    strcpy(out, __out);
 }
 
 void decode(const u16 instr, char *out) {
-    const u8 OP  = (instr & 0b1111110000000000) >> 0x10;
-    const u8 D   = (instr & 0b0000001000000000) >> 0x9;
-    const u8 W   = (instr & 0b0000000100000000) >> 0x7;
-    const u8 MOD = (instr & 0b0000000011000000) >> 0x6;
-    const u8 REG = (instr & 0b0000000000111000) >> 0x3;
+    const u8 OP  = (instr & 0b1111110000000000) >> 10;
+    const u8 D   = (instr & 0b0000001000000000) >> 9;
+    const u8 W   = (instr & 0b0000000100000000) >> 8;
+    const u8 MOD = (instr & 0b0000000011000000) >> 6;
+    const u8 REG = (instr & 0b0000000000111000) >> 3;
     const u8 RM  = (instr & 0b0000000000000111);
 
-    char reg[2];
-    regcode_to_str(REG, W, reg);
-    char rm[2];
-    regcode_to_str(RM, W, reg);
-    char op[3];
+    char op[10], reg[10], rm[10];
+
+
     instrcode_to_str(OP, op);
-    sprintf(out, "%s %s,%s", op, reg, rm);
+    regcode_to_str(REG, W, reg);
+    regcode_to_str(RM, W, rm);
+
+#ifdef DEBUG
+    printf("\n");
+    printf("MOD:"); __print_bits(MOD);
+    printf("%s:", op); __print_bits(OP);
+    printf("%s:", reg); __print_bits(REG);
+    printf("%s:", rm); __print_bits(RM);
+    printf("\n");
+#endif
+
+    const char* source = D ? rm : reg;
+    const char* destination = D ? reg : rm;
+    sprintf(out, "%s %s, %s", op, destination, source);
 }
 
+#define OUT_BUFSIZE 2048
+
 int main(int argc, const char **argv) {
+    if (argc < 2)  {
+        goto no_arg_error;
+    }
+
+    const char* filename = argv[1];
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        goto bad_path_error;
+    }
+
+    fseek(file, 0, SEEK_END);
+    u32 file_size = ftell(file);
+    rewind(file);
+
+    u8 *buf = (u8*)malloc(file_size);
+    u32 nread = 0;
+    u32 bytes_read = 0;
+    while ((nread = fread(buf, sizeof(u8), file_size, file)) > 0) {
+        bytes_read += nread;
+    }
+    if (ferror(file)) {
+        goto read_error;
+    }
+    fclose(file);
+
+    char out[OUT_BUFSIZE];
+    memset(out, 0, OUT_BUFSIZE);
+    strcat(out, "bits 16\n\n");
+
+    for (u32 i=0; i<file_size; i+=2) {
+        char line[32];
+        u16 hi = (u16)(buf[i]) << 8;
+        u16 lo = (u16)(buf[i+1]);
+        u16 instr_line = hi | lo;
+
+        decode(instr_line, line);
+        strcat(line, "\n");
+        strcat(out, line);
+    }
+
+    fprintf(stdout, out);
+    return 0;
+
+no_arg_error:
+    fprintf(stderr, "[NO_ARG_ERR]: no file arg provided\n");
+    return -1;
+bad_path_error: 
+    fprintf(stderr, "[BAD_PATH_ERR]: path %s might not exist\n", filename);
+    return -1;
+read_error:
+    fprintf(stderr, "[READ_FILE_ERR]: error reading file!\n");
+    fclose(file);
+    return -1;
 }
